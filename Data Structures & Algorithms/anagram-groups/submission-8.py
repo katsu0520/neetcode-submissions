cc9399109352d087636ec293c6695d5df4ddd46c
katsu0520 <135244672+katsu0520@users.noreply.github.com>
2026-08-21 13:55:42 +0900

    Add: anagram-groups - submission-8

---
+++ Data Structures & Algorithms/anagram-groups/submission-8.py
@@ -0,0 +1,8 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        groups = {}
+        for s in strs:
+            key = "".join(sorted(s))
+            groups[key] = groups.get(key, [])
+            groups[key].append(s)
+        return list(groups.values())
