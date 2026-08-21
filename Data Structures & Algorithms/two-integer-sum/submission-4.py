b52fe6658748679676fb156aa4c4809060d85b7d
katsu0520 <135244672+katsu0520@users.noreply.github.com>
2026-08-21 12:18:27 +0900

    Add: two-integer-sum - submission-4

---
+++ Data Structures & Algorithms/two-integer-sum/submission-4.py
@@ -0,0 +1,8 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        seen = {}
+        for i in range(len(nums)):
+            component = target - nums[i]
+            if component in seen:
+                return [seen[component],i]
+            seen[nums[i]] = i
